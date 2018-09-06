6bafb2db6acb3f63678c65a694d170e66ba74eca
andream16 <andrea.medda.uni@gmail.com>
2018-09-06 16:51:46 +0200

    Working Post and Potato Get

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,119 @@
+extern crate hyper;
+extern crate futures;
+extern crate url;
+
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+#[macro_use]
+extern crate serde_json;
+
+use std::collections::HashMap;
+use std::error::Error;
+use std::io;
+
+use hyper::server::{Request, Response, Service};
+use hyper::{Chunk, StatusCode};
+use hyper::Method::{Get, Post};
+use hyper::header::{ContentLength, ContentType};
+
+use futures::Stream;
+use futures::future::{Future, FutureResult};
+
+struct MicroService;
+
+struct NewMessage {
+    username: String,
+    message: String,
+}
+
+fn parse_form(form_chunk: Chunk) -> FutureResult<NewMessage, hyper::Error> {
+
+    let mut form = url::form_urlencoded::parse(form_chunk.as_ref())
+        .into_owned()
+        .collect::<HashMap<String, String>>();
+
+    if let Some(message) = form.remove("message") {
+        let username = form.remove("username").unwrap_or(String::from("anonymous"));
+        futures::future::ok(NewMessage {
+            username,
+            message,
+        })
+    } else {
+
+        futures::future::err(hyper::Error::from(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Missing field message",
+        )))
+    }
+}
+
+fn make_post_response(result: Result<i64, hyper::Error>) -> FutureResult<hyper::Response, hyper::Error> {
+    match result {
+        Ok(timestamp) => {
+            let payload = json!({"timestamp": timestamp}).to_string();
+            let response = Response::new()
+                .with_header(ContentLength(payload.len() as u64))
+                .with_header(ContentType::json())
+                .with_body(payload);
+            debug!("{:?}", response);
+            futures::future::ok(response)
+        }
+        Err(error) => make_error_response(error.description()),
+    }
+}
+
+fn make_error_response(error_message: &str) -> FutureResult<hyper::Response, hyper::Error> {
+    let payload = json!({"error": error_message}).to_string();
+    let response = Response::new()
+        .with_status(StatusCode::InternalServerError)
+        .with_header(ContentLength(payload.len() as u64))
+        .with_header(ContentType::json())
+        .with_body(payload);
+    debug!("{:?}", response);
+    futures::future::ok(response)
+}
+
+fn write_to_db(entry: NewMessage) -> FutureResult<i64, hyper::Error> {
+    futures::future::ok(0)
+}
+
+impl Service for MicroService {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn call(&self, request: Request) -> Self::Future {
+        match (request.method(), request.path()) {
+            (&Post, "/") => {
+                let future = request
+                    .body()
+                    .concat2()
+                    .and_then(parse_form)
+                    .and_then(write_to_db)
+                    .then(make_post_response);
+                Box::new(future)
+            }
+            (&Get, "/") => {
+                Box::new(futures::future::ok(
+                    Response::new().with_status(StatusCode::Ok)
+                ))
+            }
+            _ => Box::new(futures::future::ok(
+                Response::new().with_status(StatusCode::NotFound),
+            )),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let address = "127.0.0.1:8080".parse().unwrap();
+    let server = hyper::server::Http::new()
+        .bind(&address, || Ok(MicroService {}))
+        .unwrap();
+    info!("Running microservice at {}", address);
+    server.run().unwrap();
 }
