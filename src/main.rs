788fdb011b62597d363637c3a5192f9734e96859
andreacurve <andrea.medda@imaginecurve.com>
2018-10-06 12:29:07 +0100

    Using json logger -> slog-json

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ extern crate url;
 extern crate env_logger;
 
 #[macro_use]
-extern crate log;
+extern crate slog;
+extern crate slog_json;
+
+use slog::Drain;
+use std::sync::Mutex;
 
 #[macro_use]
 extern crate serde_derive;
@@ -15,11 +19,18 @@ extern crate serde_json;
 mod api;
 
 fn main() {
-    env_logger::init();
+
+    let root = slog::Logger::root(
+        Mutex::new(slog_json::Json::default(std::io::stderr())).map(slog::Fuse),
+        o!("version" => env!("CARGO_PKG_VERSION"))
+    );
+
     let address = "127.0.0.1:8080".parse().unwrap();
     let server = hyper::server::Http::new()
         .bind(&address, || Ok(api::MicroService {}))
         .unwrap();
-    info!("Running microservice at {}", address);
+
+    info!(root, "service running on port: {}", 8080);
+
     server.run().unwrap();
 }
